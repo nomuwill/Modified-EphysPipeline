93070142689f319791b55219aa01b28f0217c2f4
surygeng <surygeng812@gmail.com>
2022-11-03 17:18:15 -0700

    Switch to braingeneerspy package

--- src/app.py
+++ src/app.py
@@ -1,15 +1,9 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-from dash import Dash, html, dcc
-import plotly.express as px
-import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, Input, Output, ctx
 import dash_daq as daq
-from braingeneers import analysis
-import numpy as np
-from localPlot import *
-from extra import *
-import pandas as pd
+import braingeneers.utils.s3wrangler as wr
+from maxwellEphys import *
 
 # dash setting
 app = Dash(__name__)
@@ -17,92 +11,254 @@ colors = {'background': 'white',
           'borderline': 'black'}
 
 ###### variables #######
-sttc_window = 20
+sttc_delta = 20
 sttc_thr = 0.35
 fr_coef = 10
-# TODO: get the path from dropdown selection and callback functions
+# fs = 20000
+
+# TODO: get the path from callback functions of dropdowns
 path = "s3://braingeneers/ephys/2022-05-18-e-connectoid/original/data/" \
        "Trace_20220518_12_53_35_chip11350.raw.h5"
+phy_path = "s3://braingeneers/ephys/2022-05-18-e-connectoid/derived/kilosort2/" \
+           "Trace_20220518_12_53_35_chip11350_curated.zip"
 ########## end ##########
 
-ephys_data = analysis.read_phy_files(path)
-print("Recording length: {} minutes".format(ephys_data.length/1000/60))
-print("Number of neurons: ", len(ephys_data.train))    # TODO: Show these as metadata on the dashboard
+ephys_dash = MaxWellEphys(phy_path, fr_coef, sttc_delta, sttc_thr)
+fig_map, circle_colors = ephys_dash.plot_map()
+fig_raster = ephys_dash.plot_raster()
 
-spike_times = ephys_data.train
-# neuron_data = {new_cluster_id:[channel_id, (chan_pos_x, chan_pos_y), [chan_template], {channel_id:cluster_templates}]}
-neuron_data = ephys_data.neuron_data
-chn_pos = np.asarray(list(neuron_data.values()))[:, 1]
-sttc = ephys_data.spike_time_tilings(delt=sttc_window)
 
-##----- Create channel map -----##
-cluster_num = np.arange(1, len(spike_times)+1)
-fire_rate = ephys_data.rates(unit='Hz') * fr_coef
-chn_map = {"clusters": cluster_num,
-                   "x_coor": list(chn_pos[:, 0]),
-                   "y_coor": list(chn_pos[:, 1]),
-                   "fire_rate": fire_rate}
-chn_map_df = pd.DataFrame(data=chn_map)
 
-##----- Create delay pairs -----##
-paired_direction = []
-for i in range(len(spike_times) - 1):  # i, j are the indices to spike_times
-    for j in range(i + 1, len(spike_times)):
-        if sttc[i][j] >= sttc_thr:
-            lat = latency(spike_times[i], spike_times[j], threshold=sttc_window)
-            pos_count = len(list(filter(lambda x: (x >= 0), lat)))
-            if abs(pos_count - (len(lat) - pos_count)) > 0.6 * len(lat):
-                if np.mean(lat) > 0:
-                    paired_direction.append([i, j, sttc[i][j], np.mean(lat)])  # signal goes from chn_1 to chn_2
-                else:
-                    paired_direction.append([j, i, sttc[i][j], abs(np.mean(lat))])
-print(len(paired_direction))
 
-start_x = [chn_pos[p[0]][0] for p in paired_direction]
-start_y = [chn_pos[p[0]][1] for p in paired_direction]
-end_x = [chn_pos[p[1]][0] for p in paired_direction]
-end_y = [chn_pos[p[1]][1] for p in paired_direction]
-sttc_xy = [p[2] for p in paired_direction]
-delay = [p[3] for p in paired_direction]
+# ##----- Create delay pairs -----##
+# paired_direction = []
+# for i in range(len(spike_times) - 1):  # i, j are the indices to spike_times
+#     for j in range(i + 1, len(spike_times)):
+#         if sttc[i][j] >= sttc_thr:
+#             lat = latency(spike_times[i], spike_times[j], threshold=sttc_window)
+#             pos_count = len(list(filter(lambda x: (x >= 0), lat)))
+#             if abs(pos_count - (len(lat) - pos_count)) > 0.6 * len(lat):
+#                 if np.mean(lat) > 0:
+#                     paired_direction.append([i, j, sttc[i][j], np.mean(lat)])  # signal goes from chn_1 to chn_2
+#                 else:
+#                     paired_direction.append([j, i, sttc[i][j], abs(np.mean(lat))])
+# print(len(paired_direction))
+#
+# start_x = [chn_pos[p[0]][0] for p in paired_direction]
+# start_y = [chn_pos[p[0]][1] for p in paired_direction]
+# end_x = [chn_pos[p[1]][0] for p in paired_direction]
+# end_y = [chn_pos[p[1]][1] for p in paired_direction]
+# sttc_xy = [p[2] for p in paired_direction]
+# delay = [p[3] for p in paired_direction]
 
-# -------------------------- plot figures ----------------------#
-fig = px.scatter(chn_map_df, x="x_coor", y="y_coor", hover_name="cluster",
-                 size="fire_rate", width=1100, height=600)  # electrode grid = 220 x 120 (W x H)
-
-# plot arrows for the paired data
-for i in range(len(start_x)):
-    if delay[i] > 1:
-        color = 'red'
-    else:
-        color = 'purple'
-    fig.add_annotation(ax=start_x[i], ay=start_y[i], axref='x', ayref='y',
-                       x=end_x[i], y=end_y[i], xref='x', yref='y',
-                       showarrow=True, arrowhead=1, arrowwidth=sttc_xy[i] * 5, arrowcolor=color,
-                       opacity=0.5)
-fig.update_yaxes(autorange="reversed", showline=True, linewidth=1, linecolor=colors['borderline'], mirror=True)
-fig.update_xaxes(showline=True, linewidth=1, linecolor=colors['borderline'], mirror=True)
-fig.update_layout(plot_bgcolor=colors['background'],
-                  paper_bgcolor=colors['background'])
+# # -------------------------- plot figures ----------------------#
+# fig = px.scatter(chn_map_df, x="x_coor", y="y_coor", hover_name="cluster",
+#                  size="fire_rate", width=1100, height=600)  # electrode grid = 220 x 120 (W x H)
+#
+# # plot arrows for the paired data
+# for i in range(len(start_x)):
+#     if delay[i] > 1:
+#         color = 'red'
+#     else:
+#         color = 'purple'
+#     fig.add_annotation(ax=start_x[i], ay=start_y[i], axref='x', ayref='y',
+#                        x=end_x[i], y=end_y[i], xref='x', yref='y',
+#                        showarrow=True, arrowhead=1, arrowwidth=sttc_xy[i] * 5, arrowcolor=color,
+#                        opacity=0.5)
+# fig.update_yaxes(autorange="reversed", showline=True, linewidth=1, linecolor=colors['borderline'], mirror=True)
+# fig.update_xaxes(showline=True, linewidth=1, linecolor=colors['borderline'], mirror=True)
+# fig.update_layout(plot_bgcolor=colors['background'],
+#                   paper_bgcolor=colors['background'])
 
 # ------------------------- dash app ---------------------------#
 # app.layout = html.Div(style={'backgroundColor': colors['background']},
 app.layout = html.Div([
-    html.Div([
-        html.H1('Ephys Analysis Visualizer'), ]),
-    html.Div([
-        daq.BooleanSwitch(id='show_network',
-                          on=False,
-                          label="Show Network",
-                          labelPosition="top"),
-        dcc.Graph(id='electrode-map', figure=fig), ])
-])
+    html.H1("MaxWell Electrophysiology dashboard"),
+    html.Br(),
+    html.Div(children=[
+        html.Div(children=[
+            html.Label('Dataset (UUID)'),
+            dcc.Dropdown(options=['id_1', 'id_2', 'id_3'], value='id_1', id="drop_down"),
+            dcc.Dropdown(options=['id_1', 'id_2', 'id_3'], value='id_1', id="drop_down_subplot", disabled=True),
+            html.Div(id='dd-output-container'),
+            html.Br(),
+            daq.BooleanSwitch(id='show_network',
+                              on=False,
+                              label="Show Network",
+                              labelPosition="top"),
+            dcc.Graph(id='electrode-map'),
+        ], style={'padding': 10}),
+        html.Div(children=[
+            html.Div(children=[
+                html.Div(children=[
+                    html.P("Spike Template"),
+                ], style={'background-color': '#e4e7ed',
+                          'margin': '10px',
+                          'padding': '15px',
+                          'box-shadow': '2px',
+                          'width': '200px',
+                          'height': '50px',
+                          'border-style': 'groove',
+                          'text-align': 'center',
+                          }),
+                html.Div(children=[
+                    html.P("ISI"),
+                ], style={'background-color': '#e4e7ed',
+                          'margin': '10px',
+                          'padding': '15px',
+                          'box-shadow': '2px',
+                          'width': '200px',
+                          'height': '50px',
+                          'border-style': 'groove',
+                          'text-align': 'center',
+                          }),
 
-# @app.callback(
-#     Output('boolean-switch-output-1', 'children'),
-#     Input('my-boolean-switch', 'on')
-# )
-# def update_output(on):
-#     return 'The switch is {}.'.format(on)
+                html.Div(children=[
+                    html.P(children="Firing Rate", id="fire_rate"),
+                ], style={'background-color': '#e4e7ed',
+                          'margin': '10px',
+                          'padding': '15px',
+                          'box-shadow': '2px',
+                          'width': '200px',
+                          'height': '50px',
+                          'border-style': 'groove',
+                          'text-align': 'center', }),
+            ], style={'padding': 10, 'display': 'flex', 'flex-direction': 'row'}),
+
+            html.Br(),
+            html.Div([
+                html.P("Raw trace with highlighted spikes"),
+            ], style={
+                'background-color': '#e4e7ed',
+                'margin': '10px',
+                'padding': '15px',
+                'box-shadow': '2px',
+                'width': '800px',
+                'height': '50px',
+                'border-style': 'solid',
+                'text-align': 'center',
+            }),
+
+            html.Br(),
+            dcc.Graph(id='raster_plot'),
+
+        ], style={'flex-direction': 'column', 'display': 'flex'}),
+
+    ], style={'display': 'flex', 'flex-direction': 'row'}),
+
+], )
+
+
+@app.callback(
+    Output('drop_down', 'options'),
+    Input('drop_down', 'search_value')
+)
+def drop_down(search_value):
+    print(search_value)
+    print(type(search_value))
+    uuids = wr.list_directories('s3://braingeneers/ephys/')
+    return uuids
+
+# # These lines are needed when using local data
+# fs = 20  # ms as the unit
+# folder_dir = "data/example_phy_data_0518/"
+# Connectoid_0518 = SpikeData(fs, folder_dir)  # TODO: should change to braingeneers class
+# fig, circle_colors, chn_map_df = electrode_map(Connectoid_0518)
+# fig2, df = raster_plot(Connectoid_0518)
+
+# print(chn_map_df)
+# print(list(chn_map_df['fire_rate']))
+
+@app.callback(
+    Output('electrode-map', 'figure'),
+    Output('raster_plot', 'figure'),
+    Output('drop_down_subplot', 'disabled'),
+    Output('fire_rate', 'children'),
+    Output('drop_down_subplot', 'options'),
+    Input('drop_down', 'value'),
+    Input('electrode-map', 'clickData'),
+    Input('raster_plot', 'clickData'),
+)
+
+def plot_elec(value, electrode_click, raster_click):
+    # print("plot function")
+    # print(value)
+    # print(type(value))
+    original_data = []
+
+    button_id = ctx.triggered_id if not None else 'No clicks yet'
+    print(button_id)
+    print("click")
+    print(electrode_click)
+    print("raster click")
+    print(raster_click)
+    firing_rate = ''
+    subfolder_dropdown_disable = True
+    if value.startswith('s3'):
+        print("+++++++++++++++++++")
+        # print(br.datasets_electrophysiology.load_metadata('2022-05-18'))
+        # print(br.data.datasets.load_batch(value))
+        # print(wr.list_directories(value))
+        original_data = wr.list_objects(value + 'original/data/')
+        # print(value + 'original/data/')
+        print(original_data)
+
+        # TODO: build the figure base on the input data from this callback
+        if original_data:
+            subfolder_dropdown_disable = False
+            pass
+        else:
+            subfolder_dropdown_disable = True
+            # TODO: build the figures when there is no original/data/ path (can we work with such data?)
+            pass
+
+    if raster_click and button_id == 'raster_plot':
+        raster_number = raster_click['points'][0]['y']
+        ephys_dash.raster_df.loc[ephys_dash.raster_df["cluster_number"] == raster_number,
+                                 ["pos"]] = 'black'
+        fig_raster.update_traces(
+            marker=dict(
+                color=ephys_dash.raster_df['Pos'],
+            )
+        )
+        cluster_number = list(ephys_dash.chn_map_df['channel']).index(int(raster_number))
+        firing_rate = ephys_dash.chn_map_df.loc[ephys_dash.chn_map_df['channel'] ==
+                                                int(raster_number)]['fire_rate'].values[0]
+        # print("here")
+        # print(firing_rate)
+        # print(int(raster_number))
+        circle_colors[cluster_number] = '#00FF00'
+        fig_map.update_traces(
+            marker=dict(
+                color=circle_colors
+            )
+        )
+    if electrode_click and (button_id == 'electrode-map'):
+        cluster_number = int(electrode_click['points'][0]['pointNumber'])
+        circle_colors[cluster_number] = '#a3a7e4'
+        fig_raster.update_traces(
+            marker=dict(
+                color=circle_colors
+            )
+        )
+
+        raster_number = electrode_click['points'][0]['hovertext']
+        firing_rate = ephys_dash.chn_map_df.loc[ephys_dash.chn_map_df['channel'] ==
+                                                int(raster_number)]['fire_rate'].values[0]
+        ephys_dash.raster_df.loc[ephys_dash.raster_df["cluster_number"] ==
+                                 str(raster_number), ["pos"]] = 'red'
+
+        fig_raster.update_traces(
+            marker=dict(
+                color=ephys_dash.raster_df['pos'],
+            )
+        )
+    # print(firing_rate)
+    # print(type(firing_rate))
+    last_electrode_click = electrode_click
+    last_raster_click = raster_click
+    return fig_map, fig_raster, subfolder_dropdown_disable, 'Fire rate ' + str(firing_rate), original_data
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8050)  # include hot-reloading by default
---
+++ src/maxwellEphys.py
@@ -0,0 +1,157 @@
+# class of maxwell ephys data for displaying on the dashbaord
+from braingeneers import analysis
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.validators.scatter.marker import SymbolValidator
+import plotly.express as px
+
+
+class MaxWellEphys():
+    def __init__(self, phy_path, fr_coef, sttc_delta, sttc_thr, fs=20000):
+        """
+        load spike sorted data from s3 using analysis.read_phy_files()
+        :param phy_path: a s3 path
+        :return: a list of spike_times as [spike_times] and neuron_data
+        dictionary structured as {new_cluster_id:
+        [channel_id, (chan_pos_x, chan_pos_y), [chan_template], {channel_id:cluster_templates}]}
+        Generate dataframe for plotting functions
+        """
+        ephys_data = analysis.read_phy_files(phy_path)
+        # TODO: Show these as metadata on the dashboard
+        self.rec_length = ephys_data.length
+        self.spike_times = ephys_data.train
+        self.neuron_data = ephys_data.neuron_data[0]
+        print("Recording length: {} minutes".format(self.rec_length / 1000 / 60))
+        print("Number of neurons: ", len(self.spike_times))
+
+        chn_pos = np.asarray(list(self.neuron_data.values()), dtype=object)[:, 1]
+        chn_pos = np.concatenate(chn_pos).reshape(len(chn_pos), 2)
+
+        ##----- Create channel map -----##
+        cluster_num = np.arange(1, len(self.spike_times) + 1)
+        fire_rate = ephys_data.rates(unit='Hz') * fr_coef
+        chn_map = {"cluster_number": cluster_num,
+                   "pos_x": chn_pos[:, 0],
+                   "pos_y": chn_pos[:, 1],
+                   "fire_rate": fire_rate}
+        self.chn_map_df = pd.DataFrame(data=chn_map)
+        self.sttc = ephys_data.spike_time_tilings(delt=sttc_delta)
+
+        ##----- Create functional pairs -----##
+        paired_direction = {"start_cls": [], "end_cls": [],
+                            "start_pos": [], "end_pos": [],
+                            "sttc": [], "latency": []}
+        for i in range(len(self.spike_times) - 1):  # i, j are the indices to spike_times
+            for j in range(i + 1, len(self.spike_times)):
+                if self.sttc[i][j] >= sttc_thr:
+                    lat = latency(self.spike_times[i], self.spike_times[j], threshold=sttc_delta)
+                    pos_count = len(list(filter(lambda x: (x >= 0), lat)))
+                    if abs(pos_count - (len(lat) - pos_count)) > 0.8 * len(lat):
+                        if np.mean(lat) > 0:
+                            pair = [i, j, chn_pos[i], chn_pos[j], self.sttc[i][j], np.mean(lat)]
+                        else:
+                            pair = [j, i, chn_pos[j], chn_pos[i], self.sttc[i][j], abs(np.mean(lat))]
+                        for ind, k in enumerate(paired_direction.keys()):
+                            paired_direction[k].append(pair[ind])
+        self.paired_dir_df = pd.DataFrame(data=paired_direction)
+
+        ##----- Create raster -----##
+        raster = {"cluster_number": cluster_num,
+                  "spike_times": self.spike_times,
+                  "pos": list(chn_pos)}
+        print(raster)
+        self.raster_df = pd.DataFrame(data=raster)
+
+    def plot_raster(self):
+        """
+        :return: The raster plot figure and the df that the raster plot is created by, in order to change color later
+        """
+        # TODO: add firing rate subplot using moving_fr_rate (see below)
+        fig_raster = go.Figure()
+        raw_symbols = SymbolValidator().values
+
+        fig_raster.add_trace(go.Scatter(
+            x=self.raster_df['spike_times'],
+            y=self.raster_df['cluster_number'],
+            mode='markers',
+            marker=dict(
+                size=6,
+            ),
+            marker_symbol=raw_symbols[3 * 137 + 2],
+        ))
+
+        fig_raster.update_layout(autosize=False,
+                                 width=1400,
+                                 height=700,
+                                 title="Raster Plot", )
+
+        return fig_raster
+
+    def plot_map(self):
+        """
+        # TODO: allow option of showing functional network
+        plot electrode map
+        :return: a figure of the map
+        """
+        colors = {'background': 'white', 'borderline': 'black'}
+        circle_colors = ['#000000'] * self.chn_map_df['pos_x'].size
+        circle_colors[-1] = '#a3a7e4'
+
+        fig_map = px.scatter(self.chn_map_df, x="pos_x", y="pos_y", hover_name="cluster_number",
+                             size="fire_rate", width=1100, height=600, title="Electrode Map")
+
+
+
+        fig_map.update_traces(marker=dict(color=circle_colors))
+
+        fig_map.update_yaxes(autorange="reversed", showline=True, linewidth=1, linecolor=colors['borderline'],
+                             mirror=True)
+        fig_map.update_xaxes(showline=True, linewidth=1, linecolor=colors['borderline'], mirror=True)
+        fig_map.update_layout(plot_bgcolor=colors['background'],
+                              paper_bgcolor=colors['background'])
+
+        return fig_map, circle_colors
+
+
+def moving_fr_rate(spike_times: list, rec_length=None, bin_size=100):
+    spike_times_all = np.sort(np.hstack(spike_times))
+    if rec_length is None:
+        rec_length = spike_times_all[-1]
+    bin_num = int(rec_length // bin_size) + 1
+    bins = np.linspace(0, rec_length, bin_num)
+    moving = [np.histogram(spike_times_all, bins + i)[0] for i in range(bin_size)]
+    moving_fr = np.mean(moving, axis=0) / bin_size * 1000  # hz
+    return bins, moving_fr
+
+
+def latency(train_1, train_2, threshold=20):
+    """
+    Find latency of train_2 to train_1 by labeling the two spike trains.
+    If the latency is greater than the threshold, move to the next spike time.
+    The threshold can be the sttc window size.
+    :return: a list of latencies
+    """
+    label_1 = [0] * len(train_1)
+    label_2 = [1] * len(train_2)
+    train_inter = list(zip(label_1, train_1)) + list(zip(label_2, train_2))
+    train_inter.sort(key=lambda a: a[1])
+
+    lat = []
+    i, diff, thr = 0, 0, threshold
+    label = train_inter[0][0]
+    coef = 1 if label == label_1[0] else -1
+    while i < len(train_inter) - 1:
+        if train_inter[i][0] != train_inter[i + 1][0]:
+            diff = train_inter[i + 1][1] - train_inter[i][1]
+            if diff > thr:
+                i += 1
+            else:
+                if train_inter[i][0] == label:
+                    lat.append(diff * coef)
+                else:
+                    lat.append(-diff * coef)
+                i += 2
+        else:
+            i += 1
+    return lat
